2793ff8ca488613a1207ef94b5aa6a7679692989
Sylvain <sylvain.joyeux@tidewise.io>
2019-07-31 21:34:01 -0300

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace imu_aceinna_openimu;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <imu_aceinna_openimu/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    imu_aceinna_openimu::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
