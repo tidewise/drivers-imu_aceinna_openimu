8ce03d124abeaa82df89812613578a9a0cff43f0
Sylvain <sylvain.joyeux@tidewise.io>
2019-08-06 17:52:18 -0300

    set default values for Configuration
    These are the values for the unit we have

--- src/Configuration.hpp
+++ src/Configuration.hpp
@@ -5,11 +5,11 @@
 
 namespace imu_aceinna_openimu {
     struct Configuration {
-        std::string periodic_packet_type;
-        int periodic_packet_rate;
-        int acceleration_low_pass_filter;
-        int angular_velocity_low_pass_filter;
-        std::string orientation;
+        std::string periodic_packet_type = "z1";
+        int periodic_packet_rate = 10;
+        int acceleration_low_pass_filter = 25;
+        int angular_velocity_low_pass_filter = 25;
+        std::string orientation = "+X+Y+Z";
     };
 }
 
