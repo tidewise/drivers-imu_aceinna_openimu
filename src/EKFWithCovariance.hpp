30cfe2b3474e71a42effff6b19165e61f7660faa
Sylvain <sylvain.joyeux@tidewise.io>
2019-08-26 16:45:47 -0300

    define EKFWithCovariance::computeNWUPosition

--- src/EKFWithCovariance.hpp
+++ src/EKFWithCovariance.hpp
@@ -4,18 +4,36 @@
 #include <base/Float.hpp>
 #include <base/samples/RigidBodyState.hpp>
 #include <base/samples/RigidBodyAcceleration.hpp>
+#include <gps_base/UTMConverter.hpp>
 #include <base/Angle.hpp>
 #include <imu_aceinna_openimu/FilterState.hpp>
 
 namespace imu_aceinna_openimu {
     /** The firmware's EKF-with-covariance message (e3) */
     struct EKFWithCovariance {
+        /** Pose solution
+         *
+         * Note that the position field is not set. Use e.g. gps_base::UTMConverter
+         * to convert the latitude/longitude to position
+         *
+         * Fields that are set are:
+         * - position, velocity and angular velocity covariances
+         * - velocity and angular velocity
+         */
         base::samples::RigidBodyState rbs;
+
+        /** Acceleration solution
+         *
+         * Covariances are not set
+         */
         base::samples::RigidBodyAcceleration rba;
+
         FilterState filter_state;
 
         base::Angle latitude;
         base::Angle longitude;
+
+        void computeNWUPosition(gps_base::UTMConverter const& converter);
     };
 }
 
