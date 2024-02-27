5a85c4c423ca27e0e3cc6a19967c1389acf53e57
Sylvain <sylvain.joyeux@tidewise.io>
2024-02-27 11:45:50 -0300

    fix: document fields of MagneticInfo

--- src/MagneticInfo.hpp
+++ src/MagneticInfo.hpp
@@ -12,8 +12,28 @@ namespace imu_aceinna_openimu {
      */
     struct MagneticInfo {
         base::Time time;
+
+        /** Magnetic declination at the current position
+         *
+         * This is the value calculated by the IMU using its internal model.
+         * The value is ned_mag2ned, where ned_mag is the NED frame aligned with
+         * the local magnetic north and ned is the NED frame aligned with the
+         * geographic (WGS84) north
+         */
         base::Angle declination;
+
+        /** Magnetometers measurements
+         *
+         * Magnetic disturbance calibration already applied
+         */
         base::Vector3d magnetometers;
+
+        /** Euler angle measurement from accelerometers and magnetometers
+         *
+         * These values are taken directly from the EKF. They are the measured
+         * pitch and roll computed from the accelerometers, and the measured
+         * yaw computed from the magnetic field. Declination is already applied
+         */
         base::Vector3d measured_euler_angles;
     };
 }
